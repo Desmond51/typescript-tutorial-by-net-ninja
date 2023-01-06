20af37e9910a226fbf028554d7eda3a9a35c2829
Desmond <desmondtekum9@gmail.com>
2023-01-06 17:25:38 +0100

    arrays and objects

--- learning.js
+++ learning.js
@@ -4,7 +4,32 @@
 // moveOn.forEach(function(moveOn){
 //     // console.log(moveOn)
 // })
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// const circ = (diameter:number)=>{
+//     return diameter * Math.PI;
+// }
+// console.log(circ(6))
+//arrays
+// const arr = [2,3,5, 'first'];
+// arr.push('4') 
+// arr[0] = '8'
+// console.log(arr)
+// Objects
+var user = {
+    name: "Desmond",
+    age: 25,
+    mood: "Happy"
 };
-console.log(circ(6));
+user = {
+    name: "Boss",
+    age: 23,
+    mood: "Sad"
+};
+// console.log(user)
+var goods;
+var arr = [];
+var names;
+names = "Desmond";
+arr = ['name', 'look', 3, false];
+arr.push('Move');
+arr.push(0);
+console.log(arr);
--- learning.ts
+++ learning.ts
@@ -10,3 +10,31 @@
 // }
 // console.log(circ(6))
 //arrays
+// const arr = [2,3,5, 'first'];
+// arr.push('4') 
+// arr[0] = '8'
+// console.log(arr)
+
+// Objects
+let user ={
+    name:"Desmond",
+    age:25,
+    mood:"Happy"
+}
+user = {
+    name:"Boss",
+    age:23,
+    mood:"Sad"
+    
+}
+// console.log(user)
+let goods:boolean|object;
+let arr:(string|number|boolean)[]=[]
+let names:string
+
+names ="Desmond";
+arr= ['name', 'look', 3, false]
+arr.push('Move')
+arr.push(0)
+
+console.log(arr)
