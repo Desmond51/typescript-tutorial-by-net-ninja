78f6a4cb00929e2156940d7c263b12326a1cd816
Desmond <desmondtekum9@gmail.com>
2023-01-06 13:43:59 +0100

    Learning Arrays

---
+++ learning.js
@@ -0,0 +1,10 @@
+// const name1 = "Desmond";
+// const moveOn = document.querySelectorAll('input')
+// console.log(moveOn)
+// moveOn.forEach(function(moveOn){
+//     // console.log(moveOn)
+// })
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(6));
---
+++ learning.ts
@@ -0,0 +1,12 @@
+// const name1 = "Desmond";
+// const moveOn = document.querySelectorAll('input')
+// console.log(moveOn)
+// moveOn.forEach(function(moveOn){
+//     // console.log(moveOn)
+// })
+
+// const circ = (diameter:number)=>{
+//     return diameter * Math.PI;
+// }
+// console.log(circ(6))
+//arrays
