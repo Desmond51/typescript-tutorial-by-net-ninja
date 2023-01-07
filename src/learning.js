47a9c903a12be85685a9bf5775182945cb04868a
Desmond <desmondtekum9@gmail.com>
2023-01-07 11:59:02 +0100

    Function basics

---
+++ src/learning.js
@@ -0,0 +1,41 @@
+"use strict";
+// const name1 = "Desmond";
+// const moveOn = document.querySelectorAll('input')
+// console.log(moveOn)
+// moveOn.forEach(function(moveOn){
+//     // console.log(moveOn)
+// })
+// const circ = (diameter:number)=>{
+//     return diameter * Math.PI;
+// }
+// console.log(circ(6))
+//arrays
+// const arr = [2,3,5, 'first'];
+// arr.push('4') 
+// arr[0] = '8'
+// console.log(arr)
+// Objects
+// let user ={
+//     name:"Desmond",
+//     age:25,
+//     mood:"Happy"
+// }
+// user = {
+//     name:"Boss",
+//     age:23,
+//     mood:"Sad"
+//}
+// console.log(user)
+// let goods:boolean|object;
+// let arr:(string|number|boolean)[]=[]
+// let names:string
+// names ="Desmond";
+// arr= ['name', 'look', 3, false]
+// arr.push('Move')
+// arr.push(0)
+// console.log(arr)
+console.log("Hi guys");
+let greet = function (a, b, c) {
+    let d = a + b;
+    console.log(d);
+};
